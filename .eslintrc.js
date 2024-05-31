914bd72e5b3bb3f88a617df415b8f6c12ead0468
samuel-videau <samuel.videau@efrei.net>
2024-05-31 12:33:27 +1000

    chore: repo config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,13 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'import'],
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'prettier',
   ],
   root: true,
   env: {
@@ -17,9 +20,25 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'no-console': 'warn',
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          ['builtin', 'external', 'index'],
+          ['sibling', 'parent', 'internal'],
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'import/named': 'off',
+    'import/no-named-as-default': 'off',
   },
 };
