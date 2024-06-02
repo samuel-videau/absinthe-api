9fa42c205529639e63296ae5bbefb93fdb4d63bb
samuel-videau <samuel.videau@efrei.net>
2024-06-02 10:06:37 +1000

    refactor: env names changed for vercel

--- src/orm-config.ts
+++ src/orm-config.ts
@@ -4,11 +4,11 @@ config();
 
 export const ormconfig: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'postgres',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : 5432,
+  username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE || 'postgres',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: ['src/db/migrations/*.ts'],
   synchronize: true,
