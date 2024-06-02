e9c430b70ef73244f35c3702ef372def93b2198b
samuel-videau <samuel.videau@efrei.net>
2024-06-02 22:35:32 +1000

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   if (process.env.NODE_ENV === 'development') {
     const config = new DocumentBuilder()
@@ -15,12 +15,6 @@ async function bootstrap(): Promise<void> {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('docs', app, document);
   }
-
-  app.enableCors({
-    origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    credentials: true,
-  });
 
   await app.listen(process.env.PORT || 3000);
 }
