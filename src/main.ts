55b1838fecf3dbf1ef59635b19045263c291c92c
samuel-videau <samuel.videau@efrei.net>
2024-06-02 15:50:10 +1000

    refactor: swagger only in dev env

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,16 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Absinthe API')
-    .setDescription('API documentation for Absinthe application')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Absinthe API')
+      .setDescription('API documentation for Absinthe application')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('docs', app, document);
+  }
 
   await app.listen(process.env.PORT || 3000);
 }
